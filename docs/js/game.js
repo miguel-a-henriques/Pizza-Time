9a01dc5f5dcec076377eeea2754494730be75f83
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-01 15:28:02 +0000

    refactor code

--- docs/js/game.js
+++ docs/js/game.js
@@ -80,7 +80,7 @@ class Game {
             document.getElementById('timeRemaining').style.color = 'white';
         }
 
-        if (this.timer <= 0) {
+        if (this.timer === 0) {
             clearInterval();
             this.endGameVictory();
         }
@@ -119,7 +119,7 @@ class Game {
             const obstacle=this.obstacles[i];
             if (this.score >= 80) {                             // Hard
                 obstacle.move(10); // speed in px
-            } else if (this.score < 80 && this.score > 25) {    // Medium
+            } else if (this.score > 25) {    // Medium
                 obstacle.move(6); // speed in px
             } else {                                            // Easy - start
                 obstacle.move(4); // speed in px
